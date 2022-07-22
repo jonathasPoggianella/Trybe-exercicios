557dda7afa4f5324dcfaf648181ca3d1c14dd762
Jonathas Poggianella <joni.jfmg@gmail.com>
2022-07-22 12:35:05 -0300

    dia2 bloco 4 exercicios de 6 a 9

--- Fundamentos/bloco_4_introdução_a_jacascript_e_logica_de_programação/dia2/index.js
+++ Fundamentos/bloco_4_introdução_a_jacascript_e_logica_de_programação/dia2/index.js
@@ -2,20 +2,21 @@
 // console.log(numbers)
 
 
-// 2 
+// exercicio 2 
 // let soma = 0;
 // for (index = 0; index < numbers.length; index +=1 )
 // soma += numbers[index];
 // console;console.log(soma);
 
-// 3
+// exercicio3
 // let soma = 0;
 //     for (index = 0; index < numbers.length; index +=1 )
 //        soma += numbers[index];
 
 // let media = soma / numbers.length;
 // console.log(media);        
-// 4
+
+// exercicio 4
 // let soma = 0;
 //     for (index = 0; index < numbers.length; index +=1 )
 //        soma += numbers[index];
@@ -26,15 +27,64 @@
 // } else {
     //     console.log("valor menor ou igual a 20")
 // }
-// 5
+
+// exercicio 5
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let maior = 0;
+// for (let index = 0;  index < numbers.length; index += 1) {
+//     for (let index2 = 0; index2 < numbers.length; index2 += 1) {
+//         if (numbers[index] > numbers[index2] && numbers[index] > maior) {
+//             maior = numbers[index];
+//         }
+//     }
+// }
+// console.log(maior)
+
+// exercicio 6
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let impar = [];
+//     for (let index = 0; index < numbers.length; index += 1) {
+//         if (numbers[index] % 2 == 1) {
+//             impar.push(numbers[index]);
+//         } 
+//     } 
+//     console.log(impar);
+// if (impar = 0 ) {
+//     console.log("nenhum valor impar encontrado")
+//     }
+
+// exercicio 7
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let menor = 3213213132132;
+// for (let index = 0;  index < numbers.length; index += 1) {
+//     for (let index2 = 0; index2 < numbers.length; index2 += 1) {
+//         if (numbers[index] < numbers[index2] && numbers[index] < menor) {
+//             menor = numbers[index];
+//         }
+//     }
+// }
+// console.log(menor)
+
+// exercicio 8
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let _1a25 = [];
+// for (let index = 0;  index < numbers.length; index += 1) {
+//         if (numbers[index] < 25) {
+//             _1a25.push(numbers[index]);
+//         }
+//     }
+// console.log(_1a25);
+
+// exercicio 9
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let maior = 0;
+let _1a25 = [];
 for (let index = 0;  index < numbers.length; index += 1) {
-    for (let index2 = 0; index2 < numbers.length; index2 += 1) {
-        if (numbers[index] > numbers[index2] && numbers[index] > maior) {
-            maior = numbers[index];
+        if (numbers[index] < 25) {
+            _1a25.push(numbers[index]);
         }
     }
+let _1a25dividido2 = [];    
+for (i = 0; i < _1a25.length; i +=1 ) {
+    _1a25dividido2.push(_1a25[i] / 2)
 }
-console.log(maior)
-
+console.log(_1a25dividido2);
